662dd777398bf599f5ff1d7ec86874ababaa1fb8
Никита Никифоров <nsrtmp@gmail.com>
2022-08-23 01:07:57 +0300

    deleting posts added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
@@ -6,42 +7,37 @@ import './styles/App.css'
 
 
 function App() {
+
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: 'Javascript - современный язык программирования.' },
-    { id: 2, title: 'Javascript', body: 'Javascript - современный язык программирования.' },
-    { id: 3, title: 'Javascript', body: 'Javascript - современный язык программирования.' }
-  ])
-
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-
-  const addNewPost = (e) => {
-    e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
+    {
+      id: 1,
+      title: "Javascript",
+      body: "Javascript - современный язык программирования.",
+    },
+    {
+      id: 2,
+      title: "Javascript",
+      body: "Javascript - современный язык программирования.",
+    },
+    {
+      id: 3,
+      title: "Javascript",
+      body: "Javascript - современный язык программирования.",
+    },
+  ]);
+  
+  const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value={title}
-          type="text"
-          placeholder="Заголовок"
-          onChange={e => setTitle(e.target.value)}
-        />
-        <MyInput
-          value={body}
-          type="text"
-          placeholder="Содержание"
-          onChange={e => setBody(e.target.value)}
-        />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title="Список постов" />
+      <PostForm create={createPost} />
+      <PostList remove={removePost} posts={posts} title="Список постов" />
     </div>
   );
 }
